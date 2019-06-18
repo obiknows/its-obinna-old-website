e7120adbc6944bfa9dbfbce9e493eda12a673463
Obinna Nnodim <obiknows88@gmail.com>
2019-06-18 10:45:15 -0700

    updated that footer too

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
     <footer class="footer">
+        <img 
+            src="../../static/OBINNA.svg"
+            :alt="settings.site_name" 
+            class="footer-logo"
+        />
         <div class="container">
             <span v-html="settings.footer" />
         </div>
@@ -17,8 +22,14 @@ export default {
 </script>
 
 <style scoped>
+.footer-logo {
+    height: 1.5rem;
+    margin-bottom: 0.25rem;
+}
+
 .footer {
     font-size: 0.8rem;
     padding: 6rem 0;
+    text-align: center;
 }
 </style>
