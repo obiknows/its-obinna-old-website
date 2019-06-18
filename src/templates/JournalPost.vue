8270cce8e0a63a606a0ce94bec0ec650faad29ce
Obinna Nnodim <obiknows88@gmail.com>
2019-06-18 10:45:40 -0700

    changed the date in the journal posting

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -33,7 +33,7 @@ query JournalPost ($path: String!) {
   post: journalPost (path: $path) {
     title
     author
-    date (format: "D. MMMM YYYY")
+    date (format: "MMMM DD, YYYY")
     timeToRead
     content
   }
