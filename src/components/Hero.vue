70d17f796e0055a37dfa07083510df1307a29d5c
Obinna Nnodim <obiknows88@gmail.com>
2019-06-18 19:15:36 -0700

    added in the 'Work With Me' button

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,6 +2,9 @@
     <div class="hero">
         <h1 class="hero-title" v-html="settings.hero_title" />
         <h2 class="hero-subtitle" v-html="settings.hero_subtitle" />
+        <g-link class="nav__link" :to="{ name: 'contact' }">
+          <button>Work With Me</button>
+        </g-link>
     </div>
 </template>
 
@@ -15,7 +18,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .hero {
     text-align: center;
     width: 480px;
@@ -43,4 +46,24 @@ export default {
     margin: 0;
     opacity: 0.6;
 }
+
+button {
+  margin: auto;
+  padding: 1rem;
+  text-transform: uppercase;
+  color: #fafafa;
+  background-color: #219653;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 1px 1px 10px rgba(50, 50, 50, 0.4);
+  transition: .3s all ease;
+  letter-spacing: .1rem;
+  font-size: .85rem;
+  position: relative;
+  top: 0;
+  right: 0;
+  margin-top: 1rem;
+  /* padding-left: 2.5rem; */
+}
+
 </style>
